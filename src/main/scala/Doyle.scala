77cf23f0fe2ca11c099b706b847f998c35fb60e8
Niklas Gerdt <gerdtni@gmail.com>
2014-09-09 15:23:28 +0300

    pocket pairs

--- src/main/scala/Doyle.scala
+++ src/main/scala/Doyle.scala
@@ -4,8 +4,13 @@ trait Doyle {
 
   def *(p: Double, v: Double): Double = p * v
 
+  def *(p: Double, v: Double, desc: String): Double = *(p, v)
+
   def *(v: Double): Double = *(1d, v)
 
   def %(v1: Double*): Double = v1.foldLeft(0d)(_ + _)
 
+  def %(v1: Double, desc: String): Double = %(v1)
+
+  def <(b: Double, p: Double, v: Double) = 100 * p * (v - b)
 }
--- src/main/scala/SmallPP.scala
+++ src/main/scala/SmallPP.scala
@@ -9,34 +9,73 @@ object SmallPP extends App with Doyle {
     *(1d, 99), // Stack after fold
     *(1d, // Set mining vs tight raiser 146/1326 (22+, ATs+, AJo+, KQ+) is minimally profitable
       %(
-        *(1 / 7.5d, // at least set
+        *(1 / 8.5d, // at least set
           %(
             *(0.5 / 100d, 0d), // you're stacked
             *(99.5 / 100d, // set holds
-              %(
-                *(5 / 10d, 130d), // he has something
-                *(5 / 10d, 108.5d) // you win cbet
+              %(// you win cbet/small pot
+                *(5 / 10d, 108.5d),
+                *(5 / 10d, 115d)
               )
             )
           )
         ),
-        *(6.5 / 7.5d, 96.5d) // missed flop & you fold
+        *(7.5 / 8.5d, 96.5d) // missed flop & you fold
       )
-    ),
+    ) // easy to defend by limiting post flop defeat size to small pot against known set miner
+    ,
     *(1d, // Set mining vs button raiser 602/1326 (22+, A2s+, K7s+, Q7s+, J7s+, T8s+, 97s+, 87s, 76s, 65s, 54s, A2o+, K8o+, Q8o+, J8o+, T8o+, 97o+, 87o, 76o, 65o, 54o) is minimally profitable
       %(
-        *(1 / 7.5d, // at least set
+        *(1 / 8.5d, // at least set
           %(
             *(0.3 / 100d, 0d), // you're stacked
             *(99.7 / 100d, // set holds
               %(
-                *(3 / 10d, 135d), // he has something
+                *(3 / 10d, 115d), // he has something
                 *(7 / 10d, 108.5d) // you win cbet
               )
             )
           )
         ),
-        *(6.5 / 7.5d, 96.5d) // missed flop & you fold
+        *(7.5 / 8.5d,
+          96.5d) // missed flop & you fold
+      )
+    ),
+    *(1d, // Fight back vs lag button raiser 602/1326.
+      // (22+, A2s+, K7s+, Q7s+, J7s+, T8s+, 97s+, 87s, 76s, 65s, 54s, A2o+, K8o+, Q8o+, J8o+, T8o+, 97o+, 87o, 76o, 65o, 54o)
+      %(
+        *(1 / 8.5d, // at least set
+          %(
+            *(0.3 / 100d, 0d), // you're stacked
+            *(99.7 / 100d, // set holds
+              %(
+                *(3 / 10d, 115d), // he has something and plays pot control
+                *(7 / 10d, 108.5d) // you win cbet
+              )
+            )
+          )
+        ),
+        *(7.5 / 8.5d, // missed flop
+          %(
+            *(7 / 10d, 96.5d), // you fold
+            *(3 / 10d, // you push him off and win small pot or lose big one
+              %(//228, 524, 78
+                *(78 / 602d,
+                  %(
+                    *(1 / 8.5d, 55d), // try to push set out
+                    *(7.5 / 8.5d, 115d)
+                  )
+                ),
+                *(524 / 602d,
+                  %(
+                    *(13 / 100d, 55d), // try to push out two pairs and fds/sds ++
+                    *(87 / 100d, 115d)
+                  )
+                )
+              )
+            )
+          )
+        )
       )
     )
   ).foreach(println(_))
